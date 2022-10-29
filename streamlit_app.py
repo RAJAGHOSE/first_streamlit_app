5dc809bc5473b57a8cf1015a696edcbc281a27e0
RAJAGHOSE <116959547+RAJAGHOSE@users.noreply.github.com>
2022-10-29 20:41:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,5 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
+stearmlit.dataframe(my_fruit_list)
+
