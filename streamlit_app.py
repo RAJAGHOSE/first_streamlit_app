667f7e6c79946fc87b1e5968f1f59ec0e60f8c1a
RAJAGHOSE <116959547+RAJAGHOSE@users.noreply.github.com>
2022-10-29 19:47:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
-streamlit.title("Code from Python notebook in GIT")
-
-streamlit.header("Sutirtha Ghosh")
-
-streamlit.text("My page")
 
