6eec5534bc153c43dd97c2b95e3d8f78ffbffe78
RAJAGHOSE <116959547+RAJAGHOSE@users.noreply.github.com>
2022-10-30 15:22:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 #
 #
 #streamlit.header('Breakfast Menu')
@@ -16,7 +17,12 @@ import pandas
 #
 #streamlit.dataframe(my_fruit_list.loc[fruits_selected])
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#streamlit.text(fruityvice_response.json())
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
 
