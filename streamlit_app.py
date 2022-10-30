7b91403f8a1f8667da1cc46633abe76253d095df
RAJAGHOSE <116959547+RAJAGHOSE@users.noreply.github.com>
2022-10-30 15:26:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,19 +3,19 @@ import pandas
 import requests
 #
 #
-#streamlit.header('Breakfast Menu')
-#streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-#streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-#streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-#streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 #
-#streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 #
-#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 #
-#fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 #
-#streamlit.dataframe(my_fruit_list.loc[fruits_selected])
+streamlit.dataframe(my_fruit_list.loc[fruits_selected])
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
